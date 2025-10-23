57659a467b31ba3a07992e74aaac19985b884cfa
Rahul Thakur <rahulthakur77752@gmail.com>
2025-10-23 20:34:08 +0530

    Issue Removed

--- Week-4/pratice.js
+++ Week-4/pratice.js
@@ -6,5 +6,9 @@ const pi = 3.14;
 let number = 42;//Number
 let text = "Hello" // String
 let isTrue = true ; // Boolean
-let nothing = null;// Null
+let nothing = null;// Object
+let undefinedVar = undefined; // undefined
+let symbolVar = Symbol(); // Symbol
+console.log(typeof(nothing));
+console.log(typeof(undefinedVar));
 
