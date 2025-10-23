6468be870dc7134fde2ba92aa86c15e8299e1c43
Rahul Thakur <rahulthakur77752@gmail.com>
2025-10-23 20:28:17 +0530

    Checking

---
+++ Week-4/pratice.js
@@ -0,0 +1,10 @@
+// var name = "rahul";
+// // Scope issue
+let name = "Rahul";
+const pi = 3.14;
+
+let number = 42;//Number
+let text = "Hello" // String
+let isTrue = true ; // Boolean
+let nothing = null;// Null
+
